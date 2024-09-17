08b86562244fcba870a6849e5dc275ded4ad0bd6
gabriel.silva <gabriel.silva@agpr5.net>
2024-09-17 08:53:04 -0300

    finalizado primeira etapa do curso de nest

--- src/courses/courses.controller.e2e-spec.ts
+++ src/courses/courses.controller.e2e-spec.ts
@@ -1,12 +1,12 @@
-import 'dotenv/config'
-import { Test, TestingModule } from '@nestjs/testing'
-import { CoursesController } from './courses.controller'
-import { INestApplication } from '@nestjs/common'
-import { Course } from './entities/courses.entity'
-import { DataSource, DataSourceOptions } from 'typeorm'
-import { Tag } from './entities/tags.entity'
-import { CoursesModule } from './courses.module'
-import { TypeOrmModule } from '@nestjs/typeorm'
+import 'dotenv/config';
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import { Course } from './entities/courses.entity';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { Tag } from './entities/tags.entity';
+import { CoursesModule } from './courses.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import request from 'supertest';
 
 describe('CoursesController e2e tests', () => {
     let app: INestApplication;
@@ -39,12 +39,11 @@ describe('CoursesController e2e tests', () => {
         app = module.createNestApplication();
         await app.init();
 
-
         data = {
             name: 'Test Course',
             description: 'Test Course Description',
             tags: ['Test Tag', 'nest.test'],
-        }
+        };
     });
 
     beforeEach(async () => {
@@ -52,12 +51,85 @@ describe('CoursesController e2e tests', () => {
         const repository = dataSource.getRepository(Course);
         courses = await repository.find();
         await dataSource.destroy();
-    })
+    });
 
+    afterAll(async () => {
+        await module.close();
+    });
 
-    
+    describe('POST /courses', () => {
+        it('should create a new course', async () => {
+            const res = await request(app.getHttpServer())
+                .post('/courses')
+                .send(data)
+                .expect(201);
+            expect(res.body.id).toBeDefined();
+            expect(res.body.name).toEqual(data.name);
+            expect(res.body.description).toEqual(data.description);
+            expect(res.body.created_at).toBeDefined();
+            expect(res.body.tags[0].name).toEqual(data.tags[0]);
+            expect(res.body.tags[1].name).toEqual(data.tags[1]);
+        });
+    });
 
-    // it('should be defined', () => {
-    //     expect(controller).toBeDefined();
-    // });
+    describe('GET /courses', () => {
+        it('should list all courses', async () => {
+            const res = await request(app.getHttpServer())
+                .get('/courses')
+                .expect(200);
+            expect(res.body[0].id).toBeDefined();
+            expect(res.body[0].name).toEqual(data.name);
+            expect(res.body[0].description).toEqual(data.description);
+            expect(res.body[0].created_at).toBeDefined();
+            res.body.map((item) =>
+                expect(item).toEqual({
+                    id: item.id,
+                    name: item.name,
+                    description: item.description,
+                    created_at: item.created_at,
+                    tags: [...item.tags],
+                }),
+            );
+        });
+    });
+
+    describe('GET /courses/:id', () => {
+        it('should get a single course by id', async () => {
+            const res = await request(app.getHttpServer())
+                .get(`/courses/${courses[0].id}`)
+                .expect(200);
+            expect(res.body.id).toEqual(courses[0].id);
+            expect(res.body.name).toEqual(courses[0].name);
+            expect(res.body.description).toEqual(courses[0].description);
+        });
+    });
+
+    describe('PUT /courses/:id', () => {
+        it('should update a course ', async () => {
+            const updateData = {
+                name: 'new name',
+                description: 'New Course Description',
+                tags: ['one', 'two'],
+            };
+            const res = await request(app.getHttpServer())
+                .put(`/courses/${courses[0].id}`)
+                .send(updateData)
+                .expect(200);
+            expect(res.body.id).toEqual(courses[0].id);
+            expect(res.body.name).toEqual('new name');
+            expect(res.body.description).toEqual('New Course Description');
+            expect(res.body.tags).toHaveLength(2);
+            expect(res.body.tags[0].name).toEqual('one');
+            expect(res.body.tags[1].name).toEqual('two');
+        });
+    });
+
+    describe('DELETE /courses/:id', () => {
+        it('should delete a course', async () => {
+            const res = await request(app.getHttpServer())
+                .delete(`/courses/${courses[0].id}`)
+                .expect(204)
+                .expect({});
+        });
+    });
 });
